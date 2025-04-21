c1bf30a3e18b32bf5f76f5e975df603940025f96
JeranSalcedo <janraymondsalcedo@gmail.com>
2025-04-21 21:33:33 +0800

    Added status bar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,20 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen
-        name='(tabs)'
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen name='not-found' options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style='light' />
+      <Stack>
+        <Stack.Screen
+          name='(tabs)'
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name='not-found' options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 };
 
